3bd86fd0d19ed78f77f558a39e8aab2197ad0e04
kulsum_siddique <kulsumsiddiquenova@gmail.com>
2018-04-03 16:17:05 +0600

    done with Assets: CSS & JavaScript

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -23,6 +23,7 @@ class BlogController extends Controller
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
+        dump($slug, $this);
         return $this->render('blog/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'comments' => $comments,
